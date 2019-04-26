b3faf231bd71a77142950b212c5e93767b667e12
Ixeemo <35169581+Ixeemo@users.noreply.github.com>
2019-04-26 14:35:11 +0200

    Grab range constrained

--- Assets/MoveObject.cs
+++ Assets/MoveObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MoveObject : MonoBehaviour
 {   
@@ -9,29 +10,35 @@ public class MoveObject : MonoBehaviour
     public GameObject tempParent;
     public GameObject Autobot;
     public bool isSpace;
+    public bool inRange;
+
     // Start is called before the first frame update
     void Start()
     {
         item.GetComponent<Rigidbody>().useGravity = true;
         isSpace = false;
+        inRange = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        //if (Input.GetKeyDown("space") && isSpace == true)
-        //{   
-        //    isSpace = false;
+    {   
+        /*
+        if (Input.GetKeyDown("space") && isSpace == true)
+        {   
+            isSpace = false;
             
-        //}
+        }
 
-        //if (isSpace == false)
-        //{
-        //    item.GetComponent<Rigidbody>().useGravity = true;
-         //   item.GetComponent<Rigidbody>().isKinematic = false;
+        
+        if (isSpace == false)
+        {
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.GetComponent<Rigidbody>().isKinematic = false;
             
             //item.transform.parent = tempParent.transform;
-        //}
+        }
+        */
 
         if (Input.GetKeyDown("space") && isSpace == false)
         {
@@ -39,7 +46,14 @@ public class MoveObject : MonoBehaviour
             
         }
 
-        if (isSpace == true)
+        if (Math.Abs(item.transform.position.x - Autobot.transform.position.x) <=  1.55f && Math.Abs(item.transform.position.z - Autobot.transform.position.z) <= 1.55f)
+        {
+            inRange = true;
+        }
+        else { inRange = false; } 
+
+
+        if (isSpace == true && inRange == true)
         {
             item.GetComponent<Rigidbody>().useGravity = false;
             item.GetComponent<Rigidbody>().isKinematic = true;
