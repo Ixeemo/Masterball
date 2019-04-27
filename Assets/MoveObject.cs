78955702a2d0b3e023ff0d4944a40fe466fd7222
Ixeemo <35169581+Ixeemo@users.noreply.github.com>
2019-04-27 20:46:13 +0200

    2 graczy

--- Assets/MoveObject.cs
+++ Assets/MoveObject.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MoveObject : MonoBehaviour
 {   
@@ -9,20 +11,64 @@ public class MoveObject : MonoBehaviour
     public Rigidbody item;
     public GameObject tempParent;
     public GameObject Autobot;
+    public GameObject Autobot1;
     public float moveSpeed;
+    public bool isO;
     public bool isSpace;
     public bool inRange;
+    public bool inRange1;
+    public int counter;
+    public int counter1;
+    public Text countText;
     
 
     // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "bramka")
+        {
+            counter ++;
+            if (counter >= 10)
+            {
+                counter = 0;
+            }
+            PlayerPrefs.SetInt("High Score", counter);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+           
+            //Destroy(other);
+            SetCountText();
+
+        }
+
+        if (other.gameObject.tag == "bramka1")
+        {
+            counter1 ++;
+            if (counter1 >= 10)
+            {
+                counter1 = 0;
+            }
+            PlayerPrefs.SetInt("High Score 1", counter1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+           
+            //Destroy(other);
+            SetCountText();
+
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Wynik " + PlayerPrefs.GetInt("High Score").ToString() + ":" + PlayerPrefs.GetInt("High Score 1").ToString();
+    }
     
     public void Shoot()
     {
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.P))
         {  
 
-            isSpace = false;
+            isO = false;
             inRange = false; 
             
             item.GetComponent<Rigidbody>().useGravity = true;
@@ -31,7 +77,17 @@ public class MoveObject : MonoBehaviour
             
             
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {  
+
+            isSpace = false;
+            inRange1 = false; 
             
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.GetComponent<Rigidbody>().isKinematic = false;
+            item.AddForce(0, 0, moveSpeed, ForceMode.Impulse);
+        }    
     }
     
 
@@ -41,8 +97,13 @@ public class MoveObject : MonoBehaviour
     {
         item = GetComponent<Rigidbody>();
         item.GetComponent<Rigidbody>().useGravity = true;
+        isO = false;
         isSpace = false;
+        inRange1 = false;
         inRange = false;
+        counter = PlayerPrefs.GetInt("High Score");
+        counter1 = PlayerPrefs.GetInt("High Score 1");
+        SetCountText();
         
     }
 
@@ -66,9 +127,9 @@ public class MoveObject : MonoBehaviour
         }
         */
 
-        if ((Input.GetKeyDown("space") && isSpace == false) && inRange == true)
+        if ((Input.GetKeyDown(KeyCode.O) && isO == false) && inRange == true)
         {
-            isSpace = true;
+            isO = true;
             
         }
 
@@ -79,12 +140,37 @@ public class MoveObject : MonoBehaviour
         else { inRange = false; } 
 
 
-        if (isSpace == true && inRange == true)
+        if (isO == true && inRange == true)
         {
 
             item.GetComponent<Rigidbody>().useGravity = false;
             item.GetComponent<Rigidbody>().isKinematic = true;
             item.transform.position = new Vector3(Autobot.transform.position.x, Autobot.transform.position.y + 0.1f, Autobot.transform.position.z - 1.25f);
+
+            Shoot();           
+            //item.transform.parent = tempParent.transform;
+        }
+
+        // drugi gracz:
+        if ((Input.GetKeyDown(KeyCode.Space) && isSpace == false) && inRange1 == true)
+        {
+            isSpace = true;
+            
+        }
+
+        if (Math.Abs(item.transform.position.x - Autobot1.transform.position.x) <=  1.55f && Math.Abs(item.transform.position.z - Autobot1.transform.position.z) <= 1.55f)
+        {
+            inRange1 = true;
+        }
+        else { inRange1 = false; } 
+
+
+        if (isSpace == true && inRange1 == true)
+        {
+
+            item.GetComponent<Rigidbody>().useGravity = false;
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            item.transform.position = new Vector3(Autobot1.transform.position.x, Autobot1.transform.position.y + 0.1f, Autobot1.transform.position.z + 1.25f);
 
             Shoot();           
             //item.transform.parent = tempParent.transform;
---
+++ Assets/movement1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement1 : MonoBehaviour {
+
+ public float moveSpeed;
+
+ // Use this for initialization
+ void Start () 
+ {
+  
+ }
+ 
+ // Update is called once per frame
+ void Update () 
+ {
+  transform.Translate(-moveSpeed*Input.GetAxis("Vertical1")*Time.deltaTime,moveSpeed*Input.GetAxis("Horizontal1")*Time.deltaTime, 0f); 
+ }
+}﻿
