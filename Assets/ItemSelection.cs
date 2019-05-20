c190d50951cfc50cb832e0c58f630a6afa6d49be
mgozdecka16 <m.gozdecka16@gmail.com>
2019-05-20 22:01:00 +0200

    ChooseGood

---
+++ Assets/ItemSelection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ItemSelection : MonoBehaviour
+{
+
+    public Image SelectionImage;
+    public List<Sprite> ItemList = new List<Sprite>();
+    private int itemSpot = 0;
+
+    public void Next()
+    {
+        if (itemSpot < ItemList.Count - 1)
+        {
+            itemSpot++;
+            SelectionImage.sprite = ItemList[itemSpot];
+        }
+            
+    }
+
+    public void Previous()
+    {
+        if (itemSpot > 0)
+        {
+            itemSpot--;
+            SelectionImage.sprite = ItemList[itemSpot];
+        }
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
