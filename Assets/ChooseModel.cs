3dc5ac7b41ab216487f0ccdf7698273ca234918e
mgozdecka16 <m.gozdecka16@gmail.com>
2019-05-22 09:56:02 +0200

    ChoosePrefab

---
+++ Assets/ChooseModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseModel : MonoBehaviour
+{
+    private GameObject[] modelsList;
+    private int index;
+
+    private void Start()
+    {
+        modelsList = new GameObject[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+            modelsList[i] = transform.GetChild(i).gameObject;
+
+        foreach (GameObject go in modelsList)
+            go.SetActive(false);
+
+        if (modelsList[0])
+            modelsList[0].SetActive(true);
+    }
+
+    public void NextPlayer1()
+    {
+        Debug.Log("Weszlo");
+        modelsList[index].SetActive(false);
+        index++;
+        if (index == modelsList.Length-1) //czy tu ma być -1 czy nie?
+            index = 0;
+
+        modelsList[index].SetActive(true);
+    }
+
+    public void PreviousPlayer1()
+    {
+        modelsList[index].SetActive(false);
+        index--;
+        if (index < 0)
+            index = modelsList.Length - 1;
+
+        modelsList[index].SetActive(true);
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/ItemSelection.cs
+++ Assets/ItemSelection.cs
@@ -8,28 +8,54 @@ using UnityEngine.SceneManagement;
 public class ItemSelection : MonoBehaviour
 {
 
-    public Image SelectionImage;
-    public List<Sprite> ItemList = new List<Sprite>();
-    private int itemSpot = 0;
+    //public Image SelectionImagePlayer1;
+    //public Image SelectionImagePlayer2;
+    public GameObject SpaceShuttlePlayer1;
+    public List<GameObject> ItemListPlayer1 = new List<GameObject>();
+    //public List<Sprite> ItemListPlayer1 = new List<Sprite>();
+    //public List<Sprite> ItemListPlayer2 = new List<Sprite>();
+    private int itemSpotPlayer1 = 0;
+    private int itemSpotPlayer2 = 0;
 
-    public void Next()
+    public void NextPlayer1()
     {
-        if (itemSpot < ItemList.Count - 1)
+        if (itemSpotPlayer1 < ItemListPlayer1.Count - 1)
         {
-            itemSpot++;
-            SelectionImage.sprite = ItemList[itemSpot];
-        }
-            
+            itemSpotPlayer1++;
+            //SelectionImagePlayer1.sprite = ItemListPlayer1[itemSpotPlayer1];
+            SpaceShuttlePlayer1 = ItemListPlayer1[itemSpotPlayer1];
+        }            
     }
 
-    public void Previous()
+    public void PreviousPlayer1()
     {
-        if (itemSpot > 0)
+        if (itemSpotPlayer1 > 0)
         {
-            itemSpot--;
-            SelectionImage.sprite = ItemList[itemSpot];
+            itemSpotPlayer1--;
+            //SelectionImagePlayer1.sprite = ItemListPlayer1[itemSpotPlayer1];
+            SpaceShuttlePlayer1 = ItemListPlayer1[itemSpotPlayer1];
         }
     }
+
+    /*
+    public void NextPlayer2()
+    {
+        if (itemSpotPlayer2 < ItemListPlayer2.Count - 1)
+        {
+            itemSpotPlayer2++;
+            SelectionImagePlayer2.sprite = ItemListPlayer2[itemSpotPlayer2];
+        }
+    }
+
+    public void PreviousPlayer2()
+    {
+        if (itemSpotPlayer2 > 0)
+        {
+            itemSpotPlayer2--;
+            SelectionImagePlayer2.sprite = ItemListPlayer2[itemSpotPlayer2];
+        }
+    }
+    */
 
     public void Back()
     {
