1094a27afde7d00d471b24d66435094534360bff
mgozdecka16 <m.gozdecka16@gmail.com>
2019-05-27 01:09:02 +0200

    Good

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -2,28 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCamera : MonoBehaviour {
+public class MoveCamera : MonoBehaviour
+{
 
- public float moveSpeed;
- public GameObject autoBot;
- 
+    public float moveSpeed;
+
+
 
     // Use this for initialization
-    void Start () 
- {
-      
- }
- 
- // Update is called once per frame
- void FixedUpdate () 
- {  
-    //rigidbody.isKinematic = false;
-    autoBot.transform.Translate(moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime, -moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f); 
- }
+    void Start()
+    {
 
-void OnCollisionEnter(Collision collision) 
- {
-         
- }
- 
-}﻿
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //rigidbody.isKinematic = false;
+        transform.Translate(moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime, -moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+}
